f58474f8270de95dd60a329025098a2d6c2e4863
InbarHavdala <129771643+InbarHavdala@users.noreply.github.com>
2024-04-07 17:52:43 +0300

    Update rolling-update.sh

--- kubernetes/rolling-update.sh
+++ kubernetes/rolling-update.sh
@@ -4,4 +4,4 @@ kubectl scale deployment node-deployment --replicas 6 -n nodejs
 
 sleep 5
 
-kubectl set image deployment/node-deployment nodeapp=tarun/nodejs-v1:v2 -n nodejs
+kubectl set image deployment/node-deployment nodeapp=inbar/nodejs-v1:v2 -n nodejs
